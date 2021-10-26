d9b2d061d75d2e33ccbe343bb60d058cfff6ffbf
AlexanderTheGreatSK <alex.okrucky@gmail.com>
2021-10-26 15:03:23 +0200

    scanner update from scanner branch

---
+++ main.c
@@ -0,0 +1,4 @@
+//
+// Created by alex on 26. 10. 2021.
+//
+
---
+++ misc.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 26. 10. 2021.
+//
+
+#ifndef VUT_IFJ_PROJ_MISC_H
+#define VUT_IFJ_PROJ_MISC_H
+
+#endif //VUT_IFJ_PROJ_MISC_H
