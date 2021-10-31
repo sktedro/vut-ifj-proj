bc0de2d0653b7fdc12c765f70e3394b84c374283
AlexanderTheGreatSK <alex.okrucky@gmail.com>
2021-10-31 13:20:01 +0100

    working on it ;)

--- test.sh
+++ test.sh
@@ -22,5 +22,67 @@ Help()
   echo "  -s STRING     testing how scanner deals with strings"
   echo "                THIS COMMAND IS AVAILABLE JUST FOR TOKEN AS TARGET"
   echo
+  echo "  -cd CODE      testing with IFJ21 teal code"
+  echo
 
 }
+
+All() {
+  echo "$TOKEN"
+  echo "ALL"
+}
+
+Basic() {
+  echo "$TOKEN"
+  echo "ALL"
+}
+
+Code() {
+  echo "CODE"
+}
+
+Custom() {
+  echo "CUSTOM"
+}
+
+EXEC=""
+
+while [ "$#" -gt 0 ]; do
+  case $1 in
+    -h | --help)
+      help
+      exit 0
+      ;;
+    TOKEN | token)
+      TOKEN="true"
+      shift
+      ;;
+    -a | all | ALL)
+      All
+      exit 0
+      ;;
+    -b | basic | BASIC)
+      Basic
+      exit 0
+      ;;
+    -c | comment | COMMENT)
+      EXEC="${EXEC} ${1}"
+      echo "$EXEC"
+      shift
+      ;;
+    -s | string | STRING)
+        EXEC="${EXEC} ${1}"
+        echo "$EXEC"
+        shift
+        ;;
+    -cd | code | CODE)
+        Code
+        shift
+        ;;
+    *)
+      echo "ERROR"
+      exit 1;
+  esac
+done
+Custom
+
